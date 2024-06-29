0ac78f952c9a08c19397c02011cfb0baba90f31e
nirvanagit <anubhavaeron@gmail.com>
2024-06-28 17:42:28 -0700

    update command description

--- cmd/discovery.go
+++ cmd/discovery.go
@@ -21,13 +21,8 @@ import (
 // discoveryCmd represents the discovery command
 var discoveryCmd = &cobra.Command{
 	Use:   "discovery",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Discovery configuration to distribute amongst admiral operators",
+	Long:  `Discovery configuration to distribute amongst admiral operators.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		ctx := context.Background()
 		//initialize monitoring and start servers
